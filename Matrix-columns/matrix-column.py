9e051fde5837410f8e2a4dbc3fdf0019cd79f6bb
mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-25 01:46:56 +0330

    implement calculate eigen and print result

--- Matrix-columns/matrix-column.py
+++ Matrix-columns/matrix-column.py
@@ -9,7 +9,18 @@ def getinput():
         a[i] = list(map(float, input().split()))
     return a
 
+
+def calculate_eigen(a:list):
+    value, vector = np.linalg.eig(a)
+    return value, vector
+
 if __name__ == '__main__':
     a = getinput()
-    print(a)
-
+    val, vec = calculate_eigen(a)
+    for item in val:
+        print(f"{item:.3f}", end=" ")
+    print()
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            print(f"{vec[j][i]:.3f}", end=" ")
+        print()
