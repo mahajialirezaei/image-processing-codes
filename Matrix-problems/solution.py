d6615a4d8760005cd862c04084b6c82c4366eb41
mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-24 23:43:15 +0330

    bring raising exception in try block

--- Matrix-problems/solution.py
+++ Matrix-problems/solution.py
@@ -5,37 +5,35 @@ from numpy import ndarray, dtype
 
 
 def addition(m1:np.ndarray, m2:np.ndarray):
-    if m1.shape[0] != m2.shape[0] or m1.shape[1] != m2.shape[1]:
-        raise TypeError()
     try:
+        if m1.shape[0] != m2.shape[0] or m1.shape[1] != m2.shape[1]:
+            raise Exception()
         m = np.add(m1, m2)
         return m
-    except TypeError:
+    except Exception as e:
         return "Cannot be done"
 
 def subtraction(m1:np.ndarray, m2:np.ndarray):
-    if m1.shape[0] != m2.shape[0] or m1.shape[1] != m2.shape[1]:
-        raise TypeError()
     try:
+        if m1.shape[0] != m2.shape[0] or m1.shape[1] != m2.shape[1]:
+            raise Exception()
         m = np.subtract(m1, m2)
         return m
-    except TypeError:
+    except Exception as e:
         return "Cannot be done"
 
 def multiply(m1:np.ndarray, m2:np.ndarray):
-    if m1.shape[1] != m2.shape[0]:
-        raise TypeError()
     try:
-        m = np.multiply(m1, m2)
+        if m1.shape[1] != m2.shape[0]:
+            raise Exception()
+        m = np.matmul(m1, m2)
         return m
-    except TypeError:
+    except Exception as e:
         return "Cannot be done"
 
 def convolution(m1:np.ndarray, m2:np.ndarray):
-    if m1.shape[0] != m2.shape[0] or m1.shape[1] != m2.shape[1]:
-        raise TypeError()
     try:
         m = np.convolve(m1, m2, mode='valid')
         return m
-    except TypeError:
+    except Exception as e:
         return "Cannot be done"
