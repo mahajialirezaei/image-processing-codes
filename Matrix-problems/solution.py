23999539820db4c6d6a5425078fa1fc00407a3cf
mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-25 00:11:47 +0330

    implement convolution not using np.convolve

--- Matrix-problems/solution.py
+++ Matrix-problems/solution.py
@@ -28,8 +28,15 @@ def multiply(m1:np.ndarray, m2:np.ndarray):
         return "Cannot be done"
 
 def convolution(m1:np.ndarray, m2:np.ndarray):
-    try:
-        m = np.convolve(m1, m2, mode='valid')
-        return m
-    except Exception as e:
+    m_x = m1.shape[0] - m2.shape[0] + 1
+    m_y = m1.shape[1] - m2.shape[1] + 1
+    if m_x <= 0 or m_y <= 0:
         return "Cannot be done"
+    m = []
+    for i in range(m_x):
+        m.append([])
+        for j in range(m_y):
+            m[i].append([])
+            m1_s = m1[i: i+ m2.shape[0], j:j + m2.shape[1]]
+            m[i][j] = np.sum(np.multiply(m1_s, m2))
+    return np.array(m)
