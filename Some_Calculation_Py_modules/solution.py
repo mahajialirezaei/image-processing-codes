50f9428c36da04c75ef5ceb483c9b3f514d297d1
mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-27 01:18:53 +0330

    imp infinity_norm_finder

--- Some_Calculation_Py_modules/solution.py
+++ Some_Calculation_Py_modules/solution.py
@@ -1,7 +1,12 @@
 import numpy as np
+import math
+
 def eigen_finder(A: np.ndarray):
     return list() if A.shape[0] != A.shape[1] else np.linalg.eigvals(A)
 
 
 def frobenious_norm_finder(A: np.ndarray):
     return np.linalg.norm(A, 'fro')
+
+def infinity_norm_finder(A: np.ndarray):
+    return np.linalg.norm(A, math.inf)
