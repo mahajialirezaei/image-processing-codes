b4bd4ad40bbbc2bdc7bd8caa627a36487011200a
mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-27 01:26:55 +0330

    change type of return eigen_finder to list and change type and remove additional part of func min_max_normalizer

--- Some_Calculation_Py_modules/solution.py
+++ Some_Calculation_Py_modules/solution.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 def eigen_finder(A: np.array):
-    return list() if A.shape[0] != A.shape[1] else np.linalg.eigvals(A)
+    return list() if A.shape[0] != A.shape[1] else list(np.linalg.eigvals(A))
 
 
 def frobenious_norm_finder(A: np.array):
@@ -17,5 +17,4 @@ def min_max_normalizer(A: np.array):
     A_max = np.max(A)
 
     A_min_max = (A-A_max)/(A_max-A_min)
-    A_min_max = np.ndarray(A_min_max)
     return A_min_max
