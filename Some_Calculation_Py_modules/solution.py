f66c034076210fdd46cc8dd4a0aec67e2e56781d
mohammad <m.a.hajialirezaei05@gmail.com>
2025-06-27 01:17:56 +0330

    imp: frobenious_norm_finder

--- Some_Calculation_Py_modules/solution.py
+++ Some_Calculation_Py_modules/solution.py
@@ -1,3 +1,7 @@
 import numpy as np
 def eigen_finder(A: np.ndarray):
-    return list() if A.shape[0] != A.shape[1] else np.linalg.eig(A)
+    return list() if A.shape[0] != A.shape[1] else np.linalg.eigvals(A)
+
+
+def frobenious_norm_finder(A: np.ndarray):
+    return np.linalg.norm(A, 'fro')
