be6554ac04b3057c7a5fc0129912b6031af8b5ad
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-07-23 01:55:07 +0330

    edit for loop

--- Project2/Phase1/skin.py
+++ Project2/Phase1/skin.py
@@ -10,15 +10,16 @@ def detect_skin(address):
     numbers_pix = expected.shape[0] * expected.shape[1]
     for row in expected:
         for col in row:
-            for pixel in col:
-                hue_mean += pixel[0]
-                sat_mean += pixel[1]
-                val_mean += pixel[2]
+            print(col)
+            hue_mean += col[0]
+            sat_mean += col[1]
+            val_mean += col[2]
     hue_mean /= numbers_pix
     sat_mean /= numbers_pix
     val_mean /= numbers_pix
     upper_bound = [min(hue_mean * 1.2, 360), min(sat_mean * 1.2, 255) , min(val_mean * 1.2, 255)]
     lower_bound = [hue_mean * 0.8, sat_mean * 0.8, val_mean * 0.8]
     mask = cv2.inRange(img_hsv, lower_bound, upper_bound)
-
+    cv2.imshow('mask', mask)
+    cv2.show()
 detect_skin('sample/image_one.jpg')
