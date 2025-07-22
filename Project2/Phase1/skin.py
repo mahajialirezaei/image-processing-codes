c0765fd391032741d5b76dfb1c6d785b8b398eb3
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-07-23 01:53:50 +0330

    creating first mask for detecting skin

---
+++ Project2/Phase1/skin.py
@@ -0,0 +1,24 @@
+import cv2
+
+
+def detect_skin(address):
+    img = cv2.imread(address)
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    x, y = img.shape[:2]
+    expected = img_hsv[int(x//2 * 0.8):int(x//2 * 1.2), int(y//2 * 0.8):int(y//2 * 1.2)]
+    hue_mean, sat_mean, val_mean = (0, 0, 0)
+    numbers_pix = expected.shape[0] * expected.shape[1]
+    for row in expected:
+        for col in row:
+            for pixel in col:
+                hue_mean += pixel[0]
+                sat_mean += pixel[1]
+                val_mean += pixel[2]
+    hue_mean /= numbers_pix
+    sat_mean /= numbers_pix
+    val_mean /= numbers_pix
+    upper_bound = [min(hue_mean * 1.2, 360), min(sat_mean * 1.2, 255) , min(val_mean * 1.2, 255)]
+    lower_bound = [hue_mean * 0.8, sat_mean * 0.8, val_mean * 0.8]
+    mask = cv2.inRange(img_hsv, lower_bound, upper_bound)
+
+detect_skin('sample/image_one.jpg')
